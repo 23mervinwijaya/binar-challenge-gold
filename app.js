60cdff7b0a63f511f954d4d2b05f977e9474e6dc
23mervinwijaya <23mervinwijaya@gmail.com>
2024-02-28 15:09:54 +0700

    add product page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const expresslayouts = require("express-ejs-layouts");
 const morgan = require("morgan");
-
+const db = require('./db')
 const app = express();
 
 
@@ -27,11 +27,47 @@ app.use(express.urlencoded());
 
 
 // Module & Routing
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
+  const importProducts = await db("products").select('*').orderBy('nama_produk','asc').limit(5);
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
     res.render('index',{
+      title : 'Best Home Furniture in Town',
       layout : 'layouts/main-layout',
-      
+      products
     })
+})
+
+app.get('/products',async(req,res)=>{
+  const importProducts = await db("products").select('*').orderBy('nama_produk','asc');
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+    res.render('products',{
+      title : 'Best Home Furniture in Town',
+      layout : 'layouts/main-layout',
+      products
+    })
+})
+
+app.get('/product-register',async(req,res)=>{
+  const importProducts = await db("products").select('*').orderBy('nama_produk','asc')
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+  res.render('product-register',{
+    title : 'Products Edit',
+    layout : 'layouts/main-layout',
+    products,
+  })
+})
+app.get('/product=:id',async(req,res)=>{
+  const importProducts = await db("products").select('*').where('id',req.params.id).first()
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+  res.render('product-detail',{
+    title : 'Products Edit',
+    layout : 'layouts/main-layout',
+    products,
+  })
 })
 
 
@@ -43,13 +79,12 @@ app.get('/',(req,res)=>{
 
 
 
-
-
-
-
-
-
-
+app.use('/',(req,res)=>{
+  res.render('404',{
+    title : 'Page Not Found',
+    layout : 'layouts/main-layout'
+  })
+})
 
 
 // App Listener
