24fed7e113a37a8e8173180aa37ac2a18fdee64a
23mervinwijaya <23mervinwijaya@gmail.com>
2024-03-04 11:05:04 +0700

    crud 3/4 done

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-
 // Module Import
-const express = require('express');
+const express = require("express");
 const expresslayouts = require("express-ejs-layouts");
 const morgan = require("morgan");
-const db = require('./db')
+const db = require("./db");
 const app = express();
-
 
 // Port Initiate
 const port = 3000;
-
-
 
 // Middleware & View Engine
 app.set("view engine", "ejs");
@@ -19,82 +15,101 @@ app.use(express.static("Public"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
-// app.use(express.json());
+// app.use(express.json());0
 app.use(express.urlencoded());
 
-
-
-
-
 // Module & Routing
-app.get('/',async(req,res)=>{
-  const importProducts = await db("products").select('*').orderBy('nama_produk','asc').limit(5);
+app.get("/", async (req, res) => {
+  const importProducts = await db("products")
+    .select("*")
+    .orderBy("nama_produk", "asc")
+    .limit(5);
   const productRaw = JSON.stringify(importProducts);
   const products = JSON.parse(productRaw);
-    res.render('index',{
-      title : 'Best Home Furniture in Town',
-      layout : 'layouts/main-layout',
-      products
-    })
-})
-
-app.get('/products',async(req,res)=>{
-  const importProducts = await db("products").select('*').orderBy('nama_produk','asc');
-  const productRaw = JSON.stringify(importProducts);
-  const products = JSON.parse(productRaw);
-    res.render('products',{
-      title : 'Best Home Furniture in Town',
-      layout : 'layouts/main-layout',
-      products
-    })
-})
-
-app.get('/product-register',async(req,res)=>{
-  const importProducts = await db("products").select('*').orderBy('nama_produk','asc')
-  const productRaw = JSON.stringify(importProducts);
-  const products = JSON.parse(productRaw);
-  res.render('product-register',{
-    title : 'Products Edit',
-    layout : 'layouts/main-layout',
+  res.render("index", {
+    title: "Best Home Furniture in Town",
+    layout: "layouts/main-layout",
     products,
-  })
-})
-app.get('/product=:id',async(req,res)=>{
-  const importProducts = await db("products").select('*').where('id',req.params.id).first()
-  const productRaw = JSON.stringify(importProducts);
-  const products = JSON.parse(productRaw);
-  res.render('product-detail',{
-    title : 'Products Edit',
+  });
+});
+
+app.get("/add-product",(req,res)=>{
+  res.render('add-product',{
     layout : 'layouts/main-layout',
-    products,
+    title : 'Add New Product'
   })
 })
 
+app.post('/product-register',async(req,res)=>{
+  const {nama_produk,harga,deskripsi,nama_file} = req.body;
+  
+  await db("products").insert({
+    'nama_produk':nama_produk,
+    'harga':harga,
+    'deskripsi':deskripsi,
+    'nama_file':nama_file
+  })
+  res.redirect('/product-register')
+})
 
-app.delete('/delete',async(req,res)=>{
+
+app.get("/products", async (req, res) => {
+  const importProducts = await db("products")
+    .select("*")
+    .orderBy("nama_produk", "asc");
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+  res.render("products", {
+    title: "Best Home Furniture in Town",
+    layout: "layouts/main-layout",
+    products,
+  });
+});
+
+app.get("/product-register", async (req, res) => {
+  const importProducts = await db("products")
+    .select("*")
+    .orderBy("id", "asc");
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+  res.render("product-register", {
+    title: "Edit Produk",
+    layout: "layouts/main-layout",
+    products,
+  });
+});
+
+// GET Detail product
+app.get("/product-register=:id", async (req, res) => {
   const id = req.params.id;
-  console.log(id)
-  await db("products").where('id',id).del();
-  res.send(alert('berhasil'))
-})
+  const importProducts = await db("products")
+    .select("*")
+    .where("id", id)
+    .first();
+  const productRaw = JSON.stringify(importProducts);
+  const products = JSON.parse(productRaw);
+  res.render("product-detail", {
+    title: `${products.nama_produk}`,
+    layout: "layouts/main-layout",
+    products,
+  });
+});
 
+// Delete product
+app.get("/product-register/delete=:id", async (req, res) => {
+  const id = req.params.id;
+  await db("products").where("id", id).del();
+  res.redirect('/product-register')
+});
 
-
-
-
-
-
-
-
-app.use('/',(req,res)=>{
-  res.render('404',{
-    title : 'Page Not Found',
-    layout : 'layouts/main-layout'
-  })
-})
-
+app.use("/", (req, res) => {
+  res.render("404", {
+    title: "Page Not Found",
+    layout: "layouts/main-layout",
+  });
+});
 
 // App Listener
-app.listen(port,()=>{
-    console.log(`app is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
